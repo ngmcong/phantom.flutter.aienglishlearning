579a94d023a3108f88c000e8ba52e33827294eb2
phantom <ngmcong@outlook.com>
2024-12-23 15:26:18 +0700

    *

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -19,9 +19,10 @@ class MainAppState extends State<MainApp> {
   late Future<List<AIEnlighQuestion>> aiEnlighQuestions;
   bool isAnswer = false;
 
-  Future<List<AIEnlighQuestion>> generateObjects() async {
+  Future<List<AIEnlighQuestion>> generateObjects({
     String question =
-        "i have an object have fields such as: question, option 1 to 4, correctOptionIndex. create for me list 10 question fill in blank for learn english conversation for intermedial level with that object struct as json string.";
+        "i have an object have fields such as: question, option 1 to 4, correctOptionIndex. create for me a list which have greater 10 questions fill in blank for learning english with that object struct as json string.",
+  }) async {
     final response = await http.post(
       Uri.parse(
         'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAm-hDGemRSA0RA0ZfeV4n3_a5OSRLYxTw',
@@ -59,55 +60,57 @@ class MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: FutureBuilder(
-                  future: aiEnlighQuestions,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Column(
-                        children:
-                            snapshot.data!
-                                .map(
-                                  (e) => Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Flexible(
-                                            child: Text(
-                                              "${snapshot.data!.indexOf(e) + 1}. ${e.question!}",
-                                              textAlign: TextAlign.left,
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: FutureBuilder(
+                    future: aiEnlighQuestions,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          children:
+                              snapshot.data!
+                                  .map(
+                                    (e) => Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Flexible(
+                                              child: Text(
+                                                "${snapshot.data!.indexOf(e) + 1}. ${e.question!}",
+                                                textAlign: TextAlign.left,
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
                                               ),
                                             ),
-                                          ),
-                                          Visibility(
-                                            visible: isAnswer && e.isTrue(),
-                                            child: Icon(
-                                              Icons.indeterminate_check_box,
-                                              size: 20.0,
+                                            Visibility(
+                                              visible: isAnswer && e.isTrue(),
+                                              child: Icon(
+                                                Icons.indeterminate_check_box,
+                                                size: 20.0,
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Expanded(child: Text(e.option1!)),
-                                          Checkbox(
-                                            value: e.option1Checked,
-                                            onChanged: (bool? value) {
-                                              setState(() {
-                                                e.resetChecked();
-                                                e.option1Checked = true;
-                                              });
-                                            },
-                                            fillColor:
-                                                WidgetStateProperty.resolveWith(
-                                                  (states) {
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Expanded(child: Text(e.option1!)),
+                                            Checkbox(
+                                              value: e.option1Checked,
+                                              onChanged: (bool? value) {
+                                                setState(() {
+                                                  e.resetChecked();
+                                                  e.option1Checked = true;
+                                                });
+                                              },
+                                              fillColor:
+                                                  WidgetStateProperty.resolveWith((
+                                                    states,
+                                                  ) {
                                                     if (isAnswer &&
                                                         e.correctOptionIndex ==
                                                             0 &&
@@ -117,21 +120,21 @@ class MainAppState extends State<MainApp> {
                                                       return Colors.red;
                                                     }
                                                     return null;
-                                                  },
-                                                ),
-                                          ),
-                                          Expanded(child: Text(e.option2!)),
-                                          Checkbox(
-                                            value: e.option2Checked,
-                                            onChanged: (bool? value) {
-                                              setState(() {
-                                                e.resetChecked();
-                                                e.option2Checked = true;
-                                              });
-                                            },
-                                            fillColor:
-                                                WidgetStateProperty.resolveWith(
-                                                  (states) {
+                                                  }),
+                                            ),
+                                            Expanded(child: Text(e.option2!)),
+                                            Checkbox(
+                                              value: e.option2Checked,
+                                              onChanged: (bool? value) {
+                                                setState(() {
+                                                  e.resetChecked();
+                                                  e.option2Checked = true;
+                                                });
+                                              },
+                                              fillColor:
+                                                  WidgetStateProperty.resolveWith((
+                                                    states,
+                                                  ) {
                                                     if (isAnswer &&
                                                         e.correctOptionIndex ==
                                                             1 &&
@@ -141,25 +144,25 @@ class MainAppState extends State<MainApp> {
                                                       return Colors.red;
                                                     }
                                                     return null;
-                                                  },
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Expanded(child: Text(e.option3!)),
-                                          Checkbox(
-                                            value: e.option3Checked,
-                                            onChanged: (bool? value) {
-                                              setState(() {
-                                                e.resetChecked();
-                                                e.option3Checked = true;
-                                              });
-                                            },
-                                            fillColor:
-                                                WidgetStateProperty.resolveWith(
-                                                  (states) {
+                                                  }),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Expanded(child: Text(e.option3!)),
+                                            Checkbox(
+                                              value: e.option3Checked,
+                                              onChanged: (bool? value) {
+                                                setState(() {
+                                                  e.resetChecked();
+                                                  e.option3Checked = true;
+                                                });
+                                              },
+                                              fillColor:
+                                                  WidgetStateProperty.resolveWith((
+                                                    states,
+                                                  ) {
                                                     if (isAnswer &&
                                                         e.correctOptionIndex ==
                                                             2 &&
@@ -169,21 +172,21 @@ class MainAppState extends State<MainApp> {
                                                       return Colors.red;
                                                     }
                                                     return null;
-                                                  },
-                                                ),
-                                          ),
-                                          Expanded(child: Text(e.option4!)),
-                                          Checkbox(
-                                            value: e.option4Checked,
-                                            onChanged: (bool? value) {
-                                              setState(() {
-                                                e.resetChecked();
-                                                e.option4Checked = true;
-                                              });
-                                            },
-                                            fillColor:
-                                                WidgetStateProperty.resolveWith(
-                                                  (states) {
+                                                  }),
+                                            ),
+                                            Expanded(child: Text(e.option4!)),
+                                            Checkbox(
+                                              value: e.option4Checked,
+                                              onChanged: (bool? value) {
+                                                setState(() {
+                                                  e.resetChecked();
+                                                  e.option4Checked = true;
+                                                });
+                                              },
+                                              fillColor:
+                                                  WidgetStateProperty.resolveWith((
+                                                    states,
+                                                  ) {
                                                     if (isAnswer &&
                                                         e.correctOptionIndex ==
                                                             3 &&
@@ -193,33 +196,47 @@ class MainAppState extends State<MainApp> {
                                                       return Colors.red;
                                                     }
                                                     return null;
-                                                  },
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(height: 10),
-                                    ],
-                                  ),
-                                )
-                                .toList(),
-                      );
-                    } else {
-                      return Text('Generate Objects');
-                    }
-                  },
+                                                  }),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(height: 10),
+                                      ],
+                                    ),
+                                  )
+                                  .toList(),
+                        );
+                      } else {
+                        return Text('Generate Objects');
+                      }
+                    },
+                  ),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  isAnswer = true;
-                });
-              },
-              child: Text('Check answers'),
-            ),
-          ],
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isAnswer = true;
+                      });
+                    },
+                    child: Text('Check answers'),
+                  ),
+                  SizedBox(width: 5),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isAnswer = false;
+                        generateObjects(question: "make another");
+                      });
+                    },
+                    child: Text('Try a another test'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
